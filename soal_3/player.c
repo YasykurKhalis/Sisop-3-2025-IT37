126f82fc62176a526f1dc1fa7b2154a396fcabd1
YasykurKhalis <yaskurkaka@gmail.com>
2025-05-03 17:16:02 +0700

    Update player.c

--- soal_3/player.c
+++ soal_3/player.c
@@ -1,6 +1,3 @@
-// player.c (RPC Client)
-
-// soal a
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +6,6 @@
 
 #define PORT 12345
 
-// soal c
 typedef struct {
     int money;
     char equipped_weapon[50];
@@ -24,9 +20,8 @@ Player player = {
     .weapon_passive = "",
     .base_damage = 10,
     .enemies_defeated = 0
-}; // c
+};
 
-// soal e
 typedef struct {
     char name[50];
     int damage;
@@ -35,7 +30,7 @@ typedef struct {
 
 Weapon inventory[100];
 int inventory_count = 0;
-int equipped_index = -1; // e
+int equipped_index = -1;
 
 int main() {
     int sock;
@@ -45,10 +40,7 @@ int main() {
     sock = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1"); // Localhost
-
-    // connect(sock, (struct sockaddr*)&server, sizeof(server));
-    // printf("Connected to dungeon server.\n");
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
         perror("Connection failed");
@@ -57,7 +49,6 @@ int main() {
 
     printf("Connected to dungeon server.\n");
     
-    // soal b
     int running = 1;
     while (running) {
 
@@ -73,20 +64,16 @@ int main() {
 
         switch (input[0]) {
             case '1':
-                // soal c
                 printf("\n[Player Stats]\n");
                 printf("Gold             : %d gold\n", player.money);
                 printf("Equipped Weapon  : %s\n", player.equipped_weapon);
                 printf("Base Damage      : %d\n", player.base_damage);
-                printf("Kills            : %d\n", player.enemies_defeated); 
-                // soal e
+                printf("Kills            : %d\n", player.enemies_defeated);
                 if (equipped_index >= 0 && strlen(inventory[equipped_index].passive) > 0) {
                     printf("Passive: %s\n", inventory[equipped_index].passive);
-                } // e 
-                // c
+                }
                 break;
             case '2':
-                // soal d
                 send(sock, "shop", strlen("shop"), 0);
                 memset(buffer, 0, sizeof(buffer));
                 recv(sock, buffer, sizeof(buffer), 0);
@@ -105,7 +92,6 @@ int main() {
                 recv(sock, buffer, sizeof(buffer), 0);
                 printf("%s\n", buffer);
             
-                // Parsial parsing respon (jika berhasil beli, update data)
                 if (strstr(buffer, "Bought")) {
                     // Ekstrak damage & nama senjata
                     int dmg = 0;
@@ -117,7 +103,6 @@ int main() {
                         sscanf(dmg_str, "New damage: %d", &dmg);
                     }
             
-                    // Ekstrak nama senjata
                     char* start = strstr(buffer, "Bought ");
                     if (start) {
                         sscanf(start, "Bought %[^\n!]", wname);
@@ -130,7 +115,6 @@ int main() {
                         player.money = new_money;
                     }
 
-                    // soal e
                     char* p_start = strstr(buffer, "Passive: ");
                     if (p_start) {
                         sscanf(p_start, "Passive: %[^\n\r]", passive_buf);
@@ -142,9 +126,9 @@ int main() {
                         strcpy(inventory[inventory_count].passive, passive_buf); // dari parsing, bisa simpan sebelumnya
                         inventory_count++;
                         printf("Weapon '%s' added to inventory.\n", wname);
-                    } // e
+                    }
 
-                } // d
+                }
                 break;
             case '3':
                 // soal e
@@ -164,7 +148,6 @@ int main() {
                 }
             
                 printf("Equip which weapon? (index, or -1 to cancel): ");
-                // char choice[10];
                 memset(choice, 0, sizeof(choice));
 
                 fgets(choice, sizeof(choice), stdin);
@@ -177,7 +160,7 @@ int main() {
                     printf("Equipped %s!\n", player.equipped_weapon);
                 } else {
                     printf("Cancelled.\n");
-                } // e
+                }
                 break;
             case '4':
                 send(sock, "battle start", strlen("battle start"), 0);
@@ -205,7 +188,6 @@ int main() {
                     recv(sock, buffer, sizeof(buffer), 0);
                     printf("%s", buffer);
 
-                    // reward parsing
                     char* reward_str = strstr(buffer, "[REWARD:");
                     if (reward_str) {
                         int reward;
@@ -214,12 +196,10 @@ int main() {
                         printf("[INFO] You now have %d gold.\n", player.money);
                     }
 
-                    // cek apakah musuh dikalahkan
                     if (strstr(buffer, "You defeated the enemy!")) {
                         player.enemies_defeated++;
                     }
             
-                    // cek apakah musuh sudah mati
                     if (strstr(buffer, "defeated") || strstr(buffer, "fled")) {
                         break;
                     }
@@ -229,12 +209,11 @@ int main() {
                 printf("Exiting game...\n");
                 running = 0;
                 break;
-            // soal h
             default:
                 printf("Invalid option. Please choose 1-5.\n"); // h
         }
-    } // b
+    }
 
     close(sock);
     return 0;
-} // a
+}
